fc2006f3580d641a69802a2384b2f8af5f731a0c
Adrian Bjune <afbjune@gmail.com>
2018-12-21 14:22:57 -0800

    fixed dependencies

--- gan.py
+++ gan.py
@@ -1,12 +1,12 @@
 from __future__ import print_function, division
 from tensorflow import keras
 
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout
-from keras.layers import BatchNormalization, Activation, ZeroPadding2D
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.convolutional import UpSampling2D, Conv2D
-from keras.models import Sequential, Model
-from keras.optimizers import Adam
+# from keras.layers import Input, Dense, Reshape, Flatten, Dropout
+# from keras.layers import BatchNormalization, Activation, ZeroPadding2D
+# from keras.layers.advanced_activations import LeakyReLU
+# from keras.layers.convolutional import UpSampling2D, Conv2D
+# from keras.models import Sequential, Model
+# from keras.optimizers import Adam
 
 import matplotlib.pyplot as plt
 
@@ -36,7 +36,7 @@ class GAN():
         self.img_shape = (self.img_rows, self.img_cols, self.channels)
         self.latent_dim = 100
 
-        optimizer = Adam(0.0002, 0.5)
+        optimizer = keras.optimizers.Adam(0.0002, 0.5)
 
         # Build and compile the discriminator
         self.discriminator = self.build_discriminator()
@@ -48,7 +48,7 @@ class GAN():
         self.generator = self.build_generator()
 
         # The generator takes noise as input and generates imgs
-        z = Input(shape=(self.latent_dim,))
+        z = keras.layers.Input(shape=(self.latent_dim,))
         img = self.generator(z)
 
         # For the combined model we will only train the generator
@@ -59,49 +59,49 @@ class GAN():
 
         # The combined model  (stacked generator and discriminator)
         # Trains the generator to fool the discriminator
-        self.combined = Model(z, validity)
+        self.combined = keras.models.Model(z, validity)
         self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
 
 
     def build_generator(self):
 
-        model = Sequential()
+        model = keras.models.Sequential()
 
-        model.add(Dense(256, input_dim=self.latent_dim))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(Dense(512))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(Dense(1024))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(Dense(np.prod(self.img_shape), activation='tanh'))
-        model.add(Reshape(self.img_shape))
+        model.add(keras.layers.Dense(256, input_dim=self.latent_dim))
+        model.add(keras.layers.LeakyReLU(alpha=0.2))
+        model.add(keras.layers.BatchNormalization(momentum=0.8))
+        model.add(keras.layers.Dense(512))
+        model.add(keras.layers.LeakyReLU(alpha=0.2))
+        model.add(keras.layers.BatchNormalization(momentum=0.8))
+        model.add(keras.layers.Dense(1024))
+        model.add(keras.layers.LeakyReLU(alpha=0.2))
+        model.add(keras.layers.BatchNormalization(momentum=0.8))
+        model.add(keras.layers.Dense(np.prod(self.img_shape), activation='tanh'))
+        model.add(keras.layers.Reshape(self.img_shape))
 
         model.summary()
 
-        noise = Input(shape=(self.latent_dim,))
+        noise = keras.layers.Input(shape=(self.latent_dim,))
         img = model(noise)
 
-        return Model(noise, img)
+        return keras.models.Model(noise, img)
 
     def build_discriminator(self):
 
-        model = Sequential()
+        model = keras.models.Sequential()
 
-        model.add(Flatten(input_shape=self.img_shape))
-        model.add(Dense(512))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dense(256))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dense(1, activation='sigmoid'))
+        model.add(keras.layers.Flatten(input_shape=self.img_shape))
+        model.add(keras.layers.Dense(512))
+        model.add(keras.layers.LeakyReLU(alpha=0.2))
+        model.add(keras.layers.Dense(256))
+        model.add(keras.layers.LeakyReLU(alpha=0.2))
+        model.add(keras.layers.Dense(1, activation='sigmoid'))
         model.summary()
 
-        img = Input(shape=self.img_shape)
+        img = keras.layers.Input(shape=self.img_shape)
         validity = model(img)
 
-        return Model(img, validity)
+        return keras.models.Model(img, validity)
 
     def train(self, epochs, batch_size=128, sample_interval=50):
 
